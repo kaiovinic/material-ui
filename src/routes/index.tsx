8aec15888e8aa56f62c82fd5307723b1f32a2aa9
kaiovinic <kaiovini19@hotmail.com>
2024-10-03 22:18:45 -0300

    Adicionado React router dom

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,14 @@
-export {};
+import { Navigate, Route, Routes } from "react-router-dom";
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/pagina-inicial" element={<p>Página inicial</p>} />
+
+      {/* Redirect */}
+      <Route path="*" element={<Navigate to="/pagina-inicial" />} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
